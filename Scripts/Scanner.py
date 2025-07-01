700c7a51a4ec679f3cecdf12b93d71638f8679a0
SkarrWho <filipe.guardano-da-costa@supdevinci-edu.fr>
2025-07-01 10:07:13 +0200

    refonte branche

--- Scripts/Scanner.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def scan_target(target):
-    print(f"[+] Scan de {target} (top 1000 ports + versions)...")
-    result = subprocess.run(
-        ["nmap", "-Pn", "-sV", "--top-ports", "1000", target],
-        capture_output=True, text=True
-    )
-    return result.stdout
